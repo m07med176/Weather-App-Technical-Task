ad57813e3f8d8c617294bb81f0319848f013a9ac
Mohamed <dev.mohamed.arfa@gmail.com>
2023-04-16 09:36:47 +0200

    Fix Bug

---
+++ databindingview/src/androidTest/java/com/alamiya/databindingview/presentation/weatherDetails/WeatherDetailsFragmentTest.kt
@@ -0,0 +1,30 @@
+package com.alamiya.databindingview.presentation.weatherDetails
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class WeatherDetailsFragmentTest {
+
+    @Before
+    fun init() {
+
+    }
+
+    @After
+    fun reset() {
+    }
+
+    @Test
+    fun searchTask() = runBlocking {
+
+
+    }
+}
