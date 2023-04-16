8c41a54354a939af754ddb39bd52c662b1be7151
Mohamed <dev.mohamed.arfa@gmail.com>
2023-04-16 10:45:34 +0200

    UI Testing Handle

--- databindingview/src/androidTest/java/com/alamiya/databindingview/presentation/weatherDetails/WeatherDetailsFragmentTest.kt
+++ databindingview/src/androidTest/java/com/alamiya/databindingview/presentation/weatherDetails/WeatherDetailsFragmentTest.kt
@@ -1,30 +1,53 @@
 package com.alamiya.databindingview.presentation.weatherDetails
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.alamiya.databindingview.MainActivity
+import com.alamiya.databindingview.R
 import kotlinx.coroutines.runBlocking
-import org.junit.After
+import org.hamcrest.CoreMatchers.*
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class WeatherDetailsFragmentTest {
 
-    @Before
-    fun init() {
-
-    }
-
-    @After
-    fun reset() {
-    }
-
     @Test
     fun searchTask() = runBlocking {
 
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.auto_complete_text_view))
+            .perform(click())
+            .perform(ViewActions.typeText("cairo"), ViewActions.closeSoftKeyboard())
 
+        onView(withId(R.id.auto_complete_text_view)).perform(
+            click()
+        )
+
+        onView(withId(R.id.auto_complete_text_view))
+            .perform(click())
+            .perform(ViewActions.clearText())
+            .perform(ViewActions.typeText("london"), ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.auto_complete_text_view)).perform(
+            click()
+        )
+
+        onView(withId(R.id.auto_complete_text_view))
+            .perform(click())
+            .perform(ViewActions.clearText())
+            .perform(ViewActions.typeText("alex"), ViewActions.closeSoftKeyboard())
+
+        activityScenario.close()
     }
+
 }
+
